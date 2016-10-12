7dd4d83d43c35986bb010add298ee6dcffe34336
go <go@gos-imac.local>
2016-10-12 17:15:36 +0800

    Initial Commit

---
+++ basicProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  basicProduct
+//
+//  Created by go on 12/10/16.
+//  Copyright © 2016年 huyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
